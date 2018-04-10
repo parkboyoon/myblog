ba6797138d948a9b09cd951d958d6bdeb3b29da2
parkboyoon <704260475@qq.com>
2018-04-10 23:12:53 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<template>
+  <div id="app">
+    <header class="main-head">
+      <img class="logo" src="./assets/logo.png" alt="">
+      <img class="home" src="./assets/home.png" alt="">
+      <input class="search" type="text" placeholder="搜索">
+      <img class="help" src="./assets/help.png" alt="">
+      <img class="pifu" src="./assets/pifu.png" alt="">
+      <span class="login">登录</span>
+    </header>
+    <div class="topbg">
+      <img class="login" src="./assets/timg.gif" alt="">
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #f8f8f9;
+}
+.topbg{
+  position: absolute;
+  top: 0;
+}
+.topbg img{
+  width: 100%;
+}
+.main-head{
+  position: relative;
+  height: 44px;
+  width: 100%;
+  min-width: 1200px;
+}
+.logo{
+  position: absolute;
+  left: 15px;
+  height: 36px;
+  top: 4px;
+}
+.home{
+  position: absolute;
+  left: 170px;
+  height: 30px;
+  top: 7px;
+  cursor: pointer;
+}
+.search{
+  position: absolute;
+  width: 200px;
+  height: 30px;
+  top: 7px;
+  left: calc(50% - 100px);
+  border: none;
+  background: rgba(255,255,255,0.3);
+  text-align: center;
+}
+.help{
+  position: absolute;
+  right: 150px;
+  height: 30px;
+  top: 7px;
+}
+.pifu{
+  position: absolute;
+  right: 100px;
+  height: 30px;
+  top: 7px;
+}
+.login{
+  position: absolute;
+  right: 30px;
+  top: 10px;
+}
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+</style>
