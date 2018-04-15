c355e504907a421ef9461353f1c727c6eeb35408
yuri <yuwang02@wisedu.com>
2018-04-15 10:16:54 +0800

    第二次提交

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,27 @@
       </div>
       <p class="name">yuri</p>
       <p class="zuoyouming">诗和远方的田园</p>
+      <div class="btngroup">
+        <el-button type="primary" round icon="el-icon-info">关于我</el-button>
+        <el-button type="success" round icon="el-icon-star-on">伐木累</el-button>
+        <el-button type="warning" round icon="el-icon-warning">吐槽吧</el-button>
+      </div>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item  index="2">程序猿笔记</el-menu-item >
+        <el-menu-item index="3">音悦台</el-menu-item>
+        <el-menu-item index="4">影视工厂</el-menu-item>
+        <el-menu-item index="5">图书馆</el-menu-item>
+        <el-menu-item index="6">随笔</el-menu-item>
+      </el-menu>
     </div>
-    <router-view/>
+    <div class="content">
+      <router-view/>
+    </div>
+    <footer class="main_foot">
+      <p>Copyright © 2018 yuriblog.com All Rights Reserved. 备案号:鄂ICP备15020375号-1</p>
+      <p>程序:yuri博客v1.0 前端框架:Vue.js</p>
+    </footer>
   </div>
 </template>
 
@@ -24,7 +43,10 @@ export default {
   name: 'App',
   data () {
     return {
+      activeIndex: '1'
     }
+  },
+  components: {
   }
 }
 </script>
@@ -38,6 +60,38 @@ export default {
   color: #2c3e50;
   background: #f8f8f9;
   position: relative;
+}
+.main_foot{
+  width: 100%;
+  height: 80px;
+  background: #909399;
+  margin-top: 30px;
+  padding-top: 20px;
+}
+.main_foot p{
+  line-height: 30px;
+  color: #fff;
+  font-size: 14px;
+}
+.content{
+  width: 80%;
+  margin: 436px 10% 0;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  overflow: hidden;
+  background: #fff;
+  box-shadow: 0 2px 8px 0 rgba(73,80,96,0.10)
+}
+.el-menu-demo{
+  position: absolute !important;
+  bottom: 0;
+  width: 80%;
+  margin: 0 10% !important;
+}
+.btngroup{
+  position: absolute;
+  top: 280px;
+  width: 100%;
 }
 .person img{
   width: 100px;
@@ -57,7 +111,7 @@ export default {
 }
 .zuoyouming{
   position: absolute;
-  top: 235px;
+  top: 240px;
   text-align: center;
   width: 100%;
   font-size: 20px;
@@ -68,7 +122,7 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
-  height: 400px;
+  height: 480px;
   background: url(./assets/timg.jpg) no-repeat;
   background-size: cover;
 }
@@ -87,9 +141,9 @@ export default {
 }
 .home{
   position: absolute;
-  left: 170px;
-  height: 30px;
-  top: 7px;
+  left: 190px;
+  height: 24px;
+  top: 10px;
   cursor: pointer;
 }
 .search{
@@ -105,19 +159,23 @@ export default {
 .help{
   position: absolute;
   right: 150px;
-  height: 30px;
-  top: 7px;
+  height: 24px;
+  top: 10px;
+  cursor: pointer;
 }
 .pifu{
   position: absolute;
   right: 100px;
   height: 30px;
   top: 7px;
+  cursor: pointer;
 }
 .login{
   position: absolute;
-  right: 30px;
-  top: 10px;
+  right: 35px;
+  top: 15px;
+  color: #fff;
+  cursor: pointer;
 }
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,180 @@
 <template>
-  <div></div>
+  <div class="index_wrap">
+    <div class="index_left">
+      <el-card class="box-card-hot" shadow="hover">
+        <div slot="header" class="clearfix">
+          <span>热门文章</span>
+        </div>
+        <div class="text item">
+          <ul>
+            <li><span class="sort-hot">1</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">2</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">3</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">4</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">5</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">6</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">7</span> <span class="text-hot">我的第一篇</span></li>
+            <li><span class="sort-hot">8</span> <span class="text-hot">我的第一篇</span></li>
+          </ul>
+        </div>
+      </el-card>
+      <el-card class="box-card-family" shadow="hover">
+        <div slot="header" class="clearfix">
+          <span>伐木累</span>
+        </div>
+        <div class="text item">
+          <ul>
+            <li><a href="https://www.baidu.com/">百度</a></li>
+          </ul>
+        </div>
+      </el-card>
+      <el-card class="box-card-contact" shadow="hover">
+        <div slot="header" class="clearfix">
+          <span>联系我</span>
+        </div>
+        <div class="text item">
+          <ul>
+            <li>
+              <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+                <el-button><img src="../assets/qq.png" alt=""></el-button>
+              </el-tooltip>
+              <p>QQ</p>
+            </li>
+            <li>
+              <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+                <el-button><img src="../assets/weixin.png" alt=""></el-button>
+              </el-tooltip>
+              <p>微信</p>
+            </li>
+            <li>
+              <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+                <el-button><img src="../assets/weibo.png" alt=""></el-button>
+              </el-tooltip>
+              <p>微博</p>
+            </li>
+            <li>
+              <el-tooltip class="item" effect="dark" content="Top Center 提示文字" placement="top">
+                <el-button><img src="../assets/youxiang.png" alt=""></el-button>
+              </el-tooltip>
+              <p>邮箱</p>
+            </li>
+          </ul>
+        </div>
+      </el-card>
+    </div>
+    <div class="index_right">
+      <el-card class="box-card-blog" shadow="hover">
+        <h3 class="blog-title">前端初学路线指南</h3>
+        <div class="blog-content">
+          <div class="blog-img-wrap"><img src="../assets/person.jpg" alt=""></div>
+          <div class="blog-text-wrap">
+            之所以写这么长，是因为题主洋洋洒洒列了一大堆想学的东西，过于多了，贪多嚼不烂。
+
+            而且，学东西，总要分个轻重缓急的。任何一门知识，都有个层次，有些知识的前置知识多，后置知识少，就适合后学；有些知识前置知识少，后置知识多，就得先学，而且要认真学；还有些知识，和科目中的大部分东西都不相关，一时半会儿也用不上，这种就可以暂时搁不学（比如canvas）
+          </div>
+        </div>
+        <div>
+          <span></span>
+        </div>
+      </el-card>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+  export default{
+      data() {
+          return {}
+      }
+  }
+</script>
 
-<style></style>
+<style>
+  .el-button{
+    border: none;
+  }
+  .index_right{
+    float: left;
+    width: 70%;
+    padding: 0 20px 20px 50px;
+    box-sizing: border-box;
+  }
+  .box-card-blog .blog-title{
+    font-size: 28px;
+    line-height: 50px;
+    font-weight: 600;
+    letter-spacing: 5px;
+  }
+  .blog-content{
+    width: 100%;
+    height: 160px;
+    position: relative;
+    margin-top: 20px;
+  }
+  .blog-content .blog-img-wrap{
+    width: 30%;
+    height: 160px;
+    float: left;
+    border: 1px solid #000;
+    border-radius: 5px;
+  }
+  .blog-content .blog-img-wrap img{
+    width: 96%;
+    height: 96%;
+    margin: 2% 2%;
+  }
+  .blog-content .blog-text-wrap{
+    width: calc(70% - 2px);
+    float: left;
+  }
+  .index_left{
+    width: 30%;
+    float: left;
+    padding-left: 15px;
+    box-sizing: border-box;
+  }
+  .index_left .box-card-family,.index_left .box-card-contact{
+    margin-top: 20px;
+  }
+  .index_left .box-card-contact .item ul li{
+    width: 25%;
+    float: left;
+  }
+  .index_left .box-card-contact .item ul li img{
+    width: 100%;
+  }
+  .index_left .box-card-contact .item ul li p{
+    margin-top: -5px;
+    margin-bottom: 15px;
+    font-size: 14px;
+  }
+  .index_left .box-card-hot .item ul li{
+    text-align: left;
+    line-height: 30px;
+    position: relative;
+  }
+  .index_left .box-card-hot .item ul li span.sort-hot{
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: #909399;
+    display: inline-block;
+    text-align: center;
+    line-height: 22px;
+    position: absolute;
+    top: 2px;
+    color: #fff;
+  }
+  .index_left .box-card-hot .item ul li:nth-of-type(1) span.sort-hot{
+    background: #409EFF;
+  }
+  .index_left .box-card-hot .item ul li:nth-of-type(2) span.sort-hot{
+    background: #67C23A;
+  }
+  .index_left .box-card-hot .item ul li:nth-of-type(3) span.sort-hot{
+    background: #E6A23C;
+  }
+  .index_left .box-card-hot .item ul li span.text-hot{
+    margin-left: 30px;
+  }
+</style>
